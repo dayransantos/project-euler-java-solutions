319b190d0118e2b547a0149387177facc3f6151d
fantasteg@gmail.com <fantasteg@gmail.com@39dc6e68-2521-11df-a06f-09de6cf81105>
2011-03-25 14:55:07 +0000

    Main

--- trunk/ProjectEuler/src/tasks/Task_195_2.java
+++ trunk/ProjectEuler/src/tasks/Task_195_2.java
@@ -1,22 +1,20 @@
 package tasks;
 
+import static java.lang.Math.ceil;
 import static java.lang.Math.sqrt;
 
 //Answer :
-public class Task_195 implements ITask {
+public class Task_195_2 implements ITask {
 //       T(100) = 1234, T(1000) = 22767, and T(10000) = 359912.
 //       Find T(1053779).
-    private static final long LIM = 75085391;
-    private static final long n = 1053779;
 
 //    private static final int LIM = 1234;
 //    private static final int n = 100;
 
-//    private static final int LIM = 22767;
-//    private static final int n = 1000;
+    private static final int LIM = 22767;
+    private static final int n = 1000;
 
-    private static final long n2 = n * n;
-    private static final long n212 = 12 * n2;
+    private static final int n2 = n * n;
     double sq3 = sqrt(3);
 
     public void solving() {
@@ -27,10 +25,26 @@ public class Task_195 implements ITask {
             long a2 = a * a;
 
             long maxb = (a2<=12*n2) ? (a-1) : (long) ((12 * n2 - 4 * n * a * sq3) / (4 * n * sq3 - 3 * a));
-//            long minc2 = 3*a2/4;
-//            long maxc2minus = a2 + 1 - a;
-//            long maxc2plus = a2 + maxb*maxb - a*maxb;
-            for (long b = 1; b <= maxb; ++b) {
+            if (maxb < 1) break;
+
+            long maxb2 = maxb*maxb;
+
+            long minc2 = 3*a2/4;
+            long maxc2minus = a2 + 1 - a;
+            long maxc2plus = a2 + maxb2 - a*maxb;
+
+            if (2*maxb >= a) {
+                long minc = (long) ceil(sq3 * a / 2);
+                long maxcminus = (long) sqrt(a2 + 1 - a);
+
+                long maxcplus = (long) sqrt(a2 + maxb2 - a * maxb);
+
+            } else {
+
+            }
+
+//            for (long b = 1; b < a; ++b) {
+            for (long b = 1; b<a && (b*(4*n*sq3 - 3*a) < 4*n*(3*n-sq3)); ++b) {
                 long b2 = b * b;
                 long c2 = a2 + b2 - a * b;
                 double dc = sqrt(c2);
@@ -40,19 +54,11 @@ public class Task_195 implements ITask {
                     continue;
                 }
 
-//                double p = (a + b + dc);
-//                if (sq3 * a * b > 2 * n * p) {
-//                    break;
-//                }
-//                double r = sq3 * a * b / (2 * p);
-//                ++res;
-//                System.out.println(res + ": " + a + " " + b + " " + c + " " + r);
-
                 ++res;
                 System.out.println(res + ": " + a + " " + b + " " + c);
-                if (res % 1000 == 0) {
-                    System.out.println(res + ": " + a + " " + b + " " + c);
-                }
+//                } else {
+//                    break;
+//                }
             }
         }
 
@@ -60,6 +66,6 @@ public class Task_195 implements ITask {
     }
 
     public static void main(String[] args) {
-        Tester.test(new Task_195());
+        Tester.test(new Task_195_2());
     }
 }
