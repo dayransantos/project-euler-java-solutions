729d7aa5cb5b39e28d1f2dd4f810758b868d14fd
fantasteg <fantasteg@gmail.com>
2011-03-31 10:40:46 +0000

    Main

---
+++ ProjectEuler/src/tasks/Task_323.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import utils.log.Logger;
+
+//Answer :
+public class Task_323 implements ITask {
+    public static void main(String[] args) {
+        Logger.init("default.log");
+        Tester.test(new Task_323());
+        Logger.close();
+    }
+
+    int n = 32;
+
+    public void solving() {
+        System.out.println(f(0));
+    }
+
+    public double f(int k) {
+        double p = 0;
+        long c = 1;
+        for (int i = 1; i <= n; ++i) {
+            c *=
+            p += ()
+        }
+
+
+        return 0;
+    }
+}
