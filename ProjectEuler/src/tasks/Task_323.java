7c474bbbdc00f50255d2507ac76d11378a70c585
fantasteg <fantasteg@39dc6e68-2521-11df-a06f-09de6cf81105>
2011-03-31 21:29:34 +0000

    177

--- ProjectEuler/src/tasks/Task_323.java
+++ ProjectEuler/src/tasks/Task_323.java
@@ -1,7 +1,11 @@
 package tasks;
 
+import utils.OtherUtils;
 import utils.log.Logger;
 
+import java.math.BigDecimal;
+
+import static java.math.BigInteger.ONE;
 import static utils.Combinatoric.C;
 
 //Answer :
@@ -12,25 +16,39 @@ public class Task_323 implements ITask {
         Logger.close();
     }
 
-    int n = 13;
+    int n = 3;
+    double eps = 1e-12;
 
     public void solving() {
         System.out.println(f(1));
+        double res = 0;
+        for (int k = 1; ;++k) {
+            double f = f(k);
+
+            res += f;
+            System.out.println(res + ": " + f);
+            if (f < eps) break;
+        }
+        System.out.println(OtherUtils.formatDouble(res, 10));
+    }
+
+    BigDecimal[] ms = new BigDecimal[n + 1];
+    private BigDecimal M(int i) {
+        if (ms[i] == null) {
+            ms[i] = new BigDecimal(C(n, i));
+            if (i%2==0) {
+                ms[i] = ms[i].negate();
+            }
+        }
+        return ms[i];
     }
 
     public double f(int k) {
-        long den = 1<<k;
-        double pden = 1;
-        double p = 0;
-        double sign = 1;
+        BigDecimal p = BigDecimal.ZERO;
         for (int i = 1; i <= n; ++i) {
-            pden *= den;
-            double c = C(i, n).longValue()*(1<<(k-1));
-            p += sign*c/pden;
-            sign = -sign;
+            p = p.add(M(i).divide(new BigDecimal(ONE.shiftLeft(k * i))));
         }
 
-        return p;
+        return k - p.multiply(new BigDecimal(k)).doubleValue();
     }
-
 }
