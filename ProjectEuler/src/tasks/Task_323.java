35de1d2b401b62084ca5ed3e10413f8ba6d44e00
fantasteg <fantasteg@gmail.com>
2011-04-01 14:47:37 +0000

    Main

--- ProjectEuler/src/tasks/Task_323.java
+++ ProjectEuler/src/tasks/Task_323.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import static java.math.BigDecimal.ONE;
-import static java.math.BigDecimal.ZERO;
+import static utils.Combinatoric.C;
 
 //Answer :
 public class Task_323 implements ITask {
@@ -18,36 +18,49 @@ public class Task_323 implements ITask {
     }
 
     int n = 32;
-    double eps = 1e-290;
-    BigDecimal two = new BigDecimal(2);
+    double eps = 1e-18;
 
-//    11.276717951382189: 5.421010862427522E-20
-//    11.276717951382189: 2.7635739376302223E-76
+//       6.3551758451
 
     public void solving() {
         double res = 0;
-        double prevf = 0;
-        BigDecimal prevpone = ZERO;
         for (int k = 1; ;++k) {
             BigDecimal p2k = p2(k);
             BigDecimal pone = p2k.subtract(ONE).divide(p2k);
             BigDecimal p = pone.pow(n);
 
             double f = ONE.subtract(p).doubleValue();
-            f *= (1 - prevf);
+            f *= pz(k-1);
 
             res += f * k;
             System.out.println(res + ": " + f);
 
             if (f < eps) break;
-
-            prevf = f;
         }
         System.out.println(OtherUtils.formatDouble(res, 10));
     }
 
     private BigDecimal p2(int k) {
         return new BigDecimal(BigInteger.ONE.shiftLeft(k));
+    }
 
+    BigDecimal[] ms = new BigDecimal[n + 1];
+    private BigDecimal M(int i) {
+        if (ms[i] == null) {
+            ms[i] = new BigDecimal(C(n, i));
+            if (i%2==0) {
+                ms[i] = ms[i].negate();
+            }
+        }
+        return ms[i];
+    }
+
+    public double pz(int k) {
+        BigDecimal p = BigDecimal.ZERO;
+        for (int i = 1; i <= n; ++i) {
+            p = p.add(M(i).divide(p2(k*i)));
+        }
+
+        return p.doubleValue();
     }
 }
