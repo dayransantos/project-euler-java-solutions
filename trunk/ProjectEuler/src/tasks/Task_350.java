41d2d222f0a1662c13d8043cb55c692416ceb886
fantasteg@gmail.com <fantasteg@gmail.com@39dc6e68-2521-11df-a06f-09de6cf81105>
2011-09-27 15:27:17 +0000

    Main

--- trunk/ProjectEuler/src/tasks/Task_350.java
+++ trunk/ProjectEuler/src/tasks/Task_350.java
@@ -2,6 +2,10 @@ package tasks;
 
 import utils.log.Logger;
 
+import java.math.BigInteger;
+
+import static utils.MyMath.bi;
+
 //Answer :
 public class Task_350 implements ITask {
     public static void main(String[] args) {
@@ -10,7 +14,14 @@ public class Task_350 implements ITask {
         Logger.close();
     }
 
+    long G = 1000000;
+    long L = 1000000000000L;
+    BigInteger BL = bi(L);
     public void solving() {
-
+        BigInteger b = bi(2);
+        while (b.compareTo(BL) < 0) {
+//            System.out.println(b);
+            b = b.nextProbablePrime();
+        }
     }
 }
