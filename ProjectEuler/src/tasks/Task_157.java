bd3f4fc59427f0a3ac838e592be098b3065670e0
fantasteg <fantasteg@gmail.com>
2011-10-10 16:35:58 +0000

    Main

--- ProjectEuler/src/tasks/Task_157.java
+++ ProjectEuler/src/tasks/Task_157.java
@@ -3,6 +3,8 @@ package tasks;
 import utils.MyMath;
 import utils.log.Logger;
 
+import static utils.MyMath.getDivisors;
+
 //Answer :
 public class Task_157 implements ITask {
     public static void main(String[] args) {
@@ -12,67 +14,36 @@ public class Task_157 implements ITask {
     }
 
     public void solving() {
-//        1.
 //            a+b    p
 //            --- = --
 //             ab   10n
 //
-//             a+b = pk; b = pk - a;
-//             ab = 10nk;
+//             (a+b)*m = pk; b = pk/m - a;
+//             ab = 10nk/m;
 //
-//             apk - a^2 = 10nk;
+//             apk/m - a^2 = 10nk/m;
 //
-//             a^2 - pk*a + 10^n*k = 0;
+//             m*a^2 - pk*a + 10^n*k = 0;
 //
-//             D = p^2*k^2 - 4k*10^n = (pk)^2 - k*4*10^n == B^2
+//             D = p^2*k^2 - 4*m*k*10^n = (pk)^2 - k*m*4*10^n == B^2
 //
-//                 a = (pk - sqrt(D))/2
-//                 b = (pk + sqrt(D))/2
+//             a = (pk - sqrt(D))/(2*m)
+//             b = (pk + sqrt(D))/(2*m)
 //
-//             (pk)^2 = B^2 + k*4*10^n;
+//             (pk)^2 = B^2 + m*k*4*10^n;
 //
-//             (pk-B)(pk+B) = k*4*10^n = c*d;
+//             (pk-B)(pk+B) = m*k*4*10^n = c*d;
 //
 //             c = s1 + s2;
 //             d = s1 - s2;
 //
 //             s1 = pk = (c+d)/2
 //             s2 = B  = (c-d)/2
-//
-//
-//        2.
-//            a+b    p
-//            --- = --
-//             ab   10n
-//
-//             (a+b)*k = p; b = p/k - a;
-//             ab = 10n/k;
-//
-//             ap/k - a^2 = 10n/k;
-//             ap - k*a^2=10^n
-//             k*a^2 - p*a + 10^n = 0;
-//
-//             D = p^2 - 4k*10^n = (p)^2 - k*4*10^n == B^2
-//
-//                 a = (p - sqrt(D))/(2k)
-//                 b = (p + sqrt(D))/(2k)
-//
-//             (p)^2 = B^2 + k*4*10^n;
-//
-//             (p-B)(p+B) = k*4*10^n = c*d;
-//
-//             c = s1 + s2;
-//             d = s1 - s2;
-//
-//             s1 = p = (c+d)/2
-//             s2 = B = (c-d)/2
-
 
         int cnt = 0;
-        for (int k = 1; k < 100000; ++k) {
-            long k2 = k*2;
-            long n = k*4*10;
-            for (long d : MyMath.getDivisors(n)) {
+        for (int s = 1; s < 100000; ++s) {
+            long n = s*4*10;
+            for (long d : getDivisors(n)) {
                 long c = n / d;
                 if (c < d) {
                     break;
@@ -84,8 +55,14 @@ public class Task_157 implements ITask {
 
                 long pk = (c + d)/2;
                 long B = (c-d)/2;
-                if (pk%k==0 && pk%2==B%2) {
-                    long p = pk / k;
+
+                for (long k : getDivisors(pk)) {
+                    long p = pk/k;
+
+
+                }
+                if (pk%s==0 && pk%2==B%2) {
+                    long p = pk / s;
                     long a = (pk - B)/2;
                     long b = (pk + B)/2;
 
