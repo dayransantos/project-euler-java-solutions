43940f83228520708005760d6f7514bafd694b06
fantasteg <fantasteg@39dc6e68-2521-11df-a06f-09de6cf81105>
2012-09-18 06:39:01 +0000

    Main

---
+++ ProjectEuler/src/main/java/tasks/Task_394.java
@@ -0,0 +1,34 @@
+package tasks;
+
+import utils.log.Logger;
+
+//Answer :
+public class Task_394 implements ITask {
+    public static void main(String[] args) {
+        Logger.init("default.log");
+        Tester.test(new Task_394());
+        Logger.close();
+    }
+
+//    public double d = 1;
+//    public double d = 2;
+//    public double d = 7.5;
+    public double d = 40;
+
+    public long n = 3000000000L;
+
+    public long zn = (long) (n/d);
+
+    public void solving() {
+        double fk = 1;
+        double fs = 0;
+        for (long k = zn + 1; k <= n; ++k) {
+            double k2 = k*k;
+//            fk = fk + (2*k - 1)/(double)k/k;
+
+            fk = (fk*(k*k - 2*k + 2) + (2*fs + 1) + 2*zn)/k2;
+            fs += fk + 1;
+        }
+        System.out.println(fk);
+    }
+}
