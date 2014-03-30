3fcd8e666527945f24274e4422e17d657f0145e4
fantasteg <fantasteg@gmail.com>
2014-03-30 19:24:28 +0000

    Main

---
+++ ProjectEuler/src/main/opener2014/opener/Task_34.java
@@ -0,0 +1,19 @@
+package opener;
+
+import tasks.ITask;
+import tasks.Tester;
+import utils.log.Logger;
+
+//Answer :
+public class Task_34 implements ITask {
+    public static void main(String[] args) {
+        Logger.init("default.log");
+        Tester.test(new Task_34());
+        Logger.close();
+    }
+
+    public void solving() {
+
+
+    }
+}
