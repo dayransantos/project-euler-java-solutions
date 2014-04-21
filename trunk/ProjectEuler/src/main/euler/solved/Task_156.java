ffb7e0036b93773c3e8c8579c40a215725579c69
fantasteg@gmail.com <fantasteg@gmail.com@39dc6e68-2521-11df-a06f-09de6cf81105>
2014-04-21 19:23:43 +0000

    Main

--- trunk/ProjectEuler/src/main/euler/solved/Task_156.java
+++ trunk/ProjectEuler/src/main/euler/solved/Task_156.java
@@ -1,8 +1,10 @@
-package tasks;
+package solved;
 
+import tasks.ITask;
+import tasks.Tester;
 import utils.log.Logger;
 
-//Answer :
+//Answer : 21295121502550
 public class Task_156 implements ITask {
     public static void main(String[] args) {
         Logger.init("default.log");
@@ -11,50 +13,29 @@ public class Task_156 implements ITask {
     }
 
     public void solving() {
-//
-//        s(1) = 22786974071
-//
-//        50000+99982+9*4000+10000+9*300+1000+9*20+100+8*1+10+1
-//        99982+10000+1000+100+10+1
-//
-//        1*5*10^4 + 9*4*10^3 + 9*3*10^2 + 9*2*10^1 + 8*1*10^0 +
-//        99982+10000+1000+100+10+1
-//
-//        76348
-//        7*4*10^3 + 6*3*10^2 + 3*2*10^1 + 4*1*10^0 + 10000+1000+49+10+1
-//
-//        long n = 199981;
-//        long n = 100000000L;
-//        int d = 1;
-//        long res = 0;
-//        for (long i = 1; i <= n; ++i) {
-//            res += numOfDigit(i, d);
-//        }
-//        System.out.println(res);
-
         long cnt[] = new long[10];
         long res = 0;
 
         long begTime = System.currentTimeMillis();
         for (int d1 = 0; d1 < 10; ++d1) {
             System.out.println("Progress: " + d1 + " in " + (System.currentTimeMillis() - begTime) + " ms.");
-            long n1 = d1*1000000000;
+            long n1 = d1 * 1000000000L;
             for (int d2 = 0; d2 < 10; ++d2) {
-                long n2 = n1 + d2*100000000;
+                long n2 = n1 + d2 * 100000000L;
                 for (int d3 = 0; d3 < 10; ++d3) {
-                    long n3 = n2 + d3*10000000;
+                    long n3 = n2 + d3 * 10000000L;
                     for (int d4 = 0; d4 < 10; ++d4) {
-                        long n4 = n3 + d4*1000000;
+                        long n4 = n3 + d4 * 1000000L;
                         for (int d5 = 0; d5 < 10; ++d5) {
-                            long n5 = n4 + d5*100000;
+                            long n5 = n4 + d5 * 100000L;
                             for (int d6 = 0; d6 < 10; ++d6) {
-                                long n6 = n5 + d6*10000;
+                                long n6 = n5 + d6 * 10000L;
                                 for (int d7 = 0; d7 < 10; ++d7) {
-                                    long n7 = n6 + d7*1000;
+                                    long n7 = n6 + d7 * 1000L;
                                     for (int d8 = 0; d8 < 10; ++d8) {
-                                        long n8 = n7 + d8*100;
+                                        long n8 = n7 + d8 * 100L;
                                         for (int d9 = 0; d9 < 10; ++d9) {
-                                            long n9 = n8 + d9*10;
+                                            long n9 = n8 + d9 * 10L;
                                             for (int d10 = 0; d10 < 10; ++d10) {
                                                 long n10 = n9 + d10;
                                                 cnt[d1]++;
@@ -67,8 +48,16 @@ public class Task_156 implements ITask {
                                                 cnt[d8]++;
                                                 cnt[d9]++;
                                                 cnt[d10]++;
-                                                if (cnt[1] == n10) {
-                                                    System.out.println(n10);
+                                                for (int i = 1; i <= 9; ++i) {
+                                                    if (cnt[i] == n10) {
+                                                        long last = 10000000000L * (i - 1);
+                                                        for (long base = 0; base <= last; base += 10000000000L) {
+                                                            long curr = base + n10;
+
+                                                            res += curr;
+                                                            System.out.println(i + ": " + curr);
+                                                        }
+                                                    }
                                                 }
                                             }
                                         }
@@ -81,18 +70,5 @@ public class Task_156 implements ITask {
             }
         }
         System.out.println(res);
-
-        System.out.println((System.currentTimeMillis() - begTime) + " ms.");
-    }
-
-    private long numOfDigit(long n, int d) {
-        int res = 0;
-        while (n != 0) {
-            if (n % 10 == d) {
-                ++res;
-            }
-            n /= 10;
-        }
-        return res;
     }
 }
